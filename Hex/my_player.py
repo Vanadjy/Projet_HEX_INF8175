c1e95868e19d427f55e5e5aba784be33671f818c
vanadjy <valentin.dijon@free.fr>
2025-10-07 17:22:22 -0400

    Territory heuristic devised

--- Hex/my_player.py
+++ Hex/my_player.py
@@ -28,7 +28,7 @@ class MyPlayer(PlayerHex):
         
         def max_value(state: GameState, alpha, beta, depth):
             if state.is_done():
-                return (state.scores[state.players[0].id], state.scores[state.players[1].id])
+                return (state.scores[state.players[1].id], state.scores[state.players[0].id])
             elif (not state.is_done()) and depth == 0: # Si l'on a atteint la profondeur max à un état non terminal
                 return (heuristic(state), -heuristic(state)) # Jeu à somme nulle : le score d'un joueur est l'opposé de l'autre
             else:
@@ -47,7 +47,7 @@ class MyPlayer(PlayerHex):
 
         def min_value(state: GameState, alpha, beta, depth):
             if state.is_done():
-                return (state.scores[state.players[0].id], state.scores[state.players[1].id])
+                return (state.scores[state.players[1].id], state.scores[state.players[0].id])
             elif (not state.is_done()) and depth == 0: # Si l'on a atteint la profondeur max à un état non terminal
                 return (heuristic(state), -heuristic(state)) # Jeu à somme nulle : le score d'un joueur est l'opposé de l'autre
             else:
@@ -62,17 +62,40 @@ class MyPlayer(PlayerHex):
                         beta = min(beta, v_star)
                         if v_star <= alpha:
                             return (v_star, m_star)
-                return (v_star, m_star)        
+                return (v_star, m_star)
         return max_value(currentState, -math.inf, math.inf, 3)[1]
 
     def naive_heuristic(self, state: GameState):
         return 0 if not state.is_done() else state.get_player_score(self)
 
-    def heuristic_occupy_space(self, state: GameState):
-        # Pour toute pièce sous notre contrôle, on calcule le nombre de voisins libres
+    def heuristic_territory(self, state: GameState):
+        # Pour toute pièce sous notre contrôle, on calcule le nombre de voisins libres = calcul du territoire
         current_rep = state.get_rep()
-        current_env = current_rep.get_env()
-        return 0
+        territory = 0
+        if self.piece_type == "R": #i.e. we are the first player to play
+            # Get all the red cases already played
+            nb_red_cases, red_cases = current_rep.get_pieces_player(state.players[0])
+            visited_empty_tiles = set()
+            for red_case in red_cases:
+                # Get neighbours list for each red tile
+                neighbours = state.get_neighbours(red_case[0], red_case[1])
+                for n_type, (ni, nj) in neighbours.values():
+                    if n_type == "EMPTY" and (ni, nj) not in visited_empty_tiles:
+                        visited_empty_tiles.add((ni, nj))
+                        territory += 1
+        else:
+            # Get all the blue cases already played
+            nb_blue_cases, blue_cases = current_rep.get_pieces_player(state.players[1])
+            visited_empty_tiles = set()
+            for blue_case in blue_cases:
+                # Get neighbours list for each blue tile
+                neighbours = state.get_neighbours(blue_case[0], blue_case[1])
+                for n_type, (ni, nj) in neighbours.values():
+                    if n_type == "EMPTY" and (ni, nj) not in visited_empty_tiles:
+                        visited_empty_tiles.add((ni, nj))
+                        territory += 1
+        #print(territory)
+        return territory
 
     def compute_action(self, current_state: GameState, remaining_time: int = 1e9, **kwargs) -> Action:
         """
@@ -82,9 +105,9 @@ class MyPlayer(PlayerHex):
             current_state (GameState): The current game state.
 
         Returns:
-            Action: The best action as determined by minimax.
+            Action: The best action possible.
         """
-        return self.halpha_beta_strat(current_state, heuristic=self.heuristic_occupy_space)
+        return self.halpha_beta_strat(current_state, heuristic=self.heuristic_territory)
 
 
         
